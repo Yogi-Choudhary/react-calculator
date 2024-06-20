338052640616569ad620d67c5ac64e158d92f86a
Yogi-Choudhary <yogichoudhary9715@gmail.com>
2024-06-20 13:18:21 +0530

    add show user result for calculation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
+import { useState } from "react";
 import Header from "./components/Header"
 import Userinput from "./components/Userinput"
+import Results from "./components/Results";
 function App() {
+
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 1000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  function handleChange(inputIdentifier, newValue){
+    setUserInput(prevUserInput => {
+      return{
+        ...prevUserInput,
+        [inputIdentifier]: newValue
+      }
+    });
+  }
+
   return (
     <>
       <Header />
-      <Userinput />
+      <Userinput userInput={userInput} onChange={handleChange} />
+      <Results input={userInput}/>
     </>
   )
 }
